567b523aa185cf03fafcd614ff3b90969d80fa8d
Kyle Mak <kjmmak@gmail.com>
2023-12-15 10:05:34 -0800

    adjusted index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import qaController from './controllers/qaController';
 
 const express = require('express');
-const cors = require('cors');
-require('./db');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 // const router = require('./controllers/qaController');
